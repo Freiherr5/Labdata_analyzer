4320c3c0e9ad75e93c975ca9550d6ac8f691c603
Freiherr5 <wernerfeiler@gmx.de>
2022-12-02 13:57:00 +0100

    development

--- SEC_mass_picker.py
+++ SEC_mass_picker.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from configparser import ConfigParser
 import math
 
-def standard_weight_SEC(path, file_name, V0, V_total, color, heading):
+def standard_weight_SEC(path, file_name, V0, V_total, color, heading, buffer, type):
     df_mass_standard = pd.read_csv(path + file_name + ".txt")
 
     # addition of new parameters to the dataframe; base form (name, abbreviation, MW, ml)
@@ -33,14 +33,14 @@ def standard_weight_SEC(path, file_name, V0, V_total, color, heading):
     ax.text(x_max, y_max, "Index", fontsize = 10, fontweight = "semibold", ha= "left")
     ax.set_title(heading, fontsize=25)
     ax.set_ylabel("$K_{av}$", fontsize=15)
-    ax.set_xlabel("molecular weight [Da]", fontsize=15)
+    ax.set_xlabel("Molecular Weight [Da]", fontsize=15)
 
     y = df_mass_standard["Kav"].to_numpy()
     x = df_mass_standard["logMW"].to_numpy()
     m, b = np.polyfit(x, y, 1)
-    regression_formula = str(m) + " * x + " + str(b)
-    plt.plot(x, round(m, 3)*x+round(b,3), "--", color = color)
-    plt.text(float(df_mass_standard["logMW"].max()), float(df_mass_standard["Kav"].max()), "regression: " + str(regression_formula), ha= "right")
+    regression_formula = str(round(m, 3)) + " * x + " + str(round(b,3))
+    plt.plot(x, m*x+b, "--", color = color)
+    plt.text(float(df_mass_standard["logMW"].max()), float(df_mass_standard["Kav"].max()), "Regression: " + str(regression_formula), ha= "right")
     plt.xticks([1.0, 2.0, 3.0], ["10,000 Da", "100,000 Da", "1,000,000 Da"])
 
     i = 0
@@ -48,9 +48,10 @@ def standard_weight_SEC(path, file_name, V0, V_total, color, heading):
         # text above each datapoint
         ax.text(df_mass_standard.iloc[i, 5], df_mass_standard.iloc[i, 4]+df_mass_standard.iloc[0, 4]*0.01, df_mass_standard.iloc[i, 1], fontsize = 10, ha="center")
         # add legend on the top right to show context
-        ax.text(x_max, y_max - (y_max*((1.5+i)/85)), df_mass_standard.iloc[i, 1] + " = " + df_mass_standard.iloc[i, 0], fontsize = 7, ha = "left")
+        ax.text(x_max, y_max - (y_max*((1.5+i)/50)), df_mass_standard.iloc[i, 1] + " = " + df_mass_standard.iloc[i, 0] + " (" + str(df_mass_standard.iloc[i, 2]) + ")", fontsize = 10, ha = "left")
         i = i+1
-
+    ax.text(x_max, y_max - (y_max * ((1.5 + i+3) / 50)), "Column: " + type, fontsize=10, ha="left")
+    ax.text(x_max, y_max - (y_max * ((1.5 + i+4) / 50)), "Buffer:" + config[buffer]["run_b"], fontsize=10, ha="left")
     plt.savefig(str(path) + str(file_name) + ".png", dpi=400, bbox_inches="tight")
 
 
@@ -71,4 +72,4 @@ type = config["HPLC_config"]["type"]
 buffer = config["HPLC_config"]["buffer"]
 color = config["HPLC_config"]["graph_color"]
 
-standard_weight_SEC(path = path, file_name = file_name, heading = heading, color = color, V0 = V0, V_total = V_total)
+standard_weight_SEC(path = path, file_name = file_name, heading = heading, color = color, V0 = V0, V_total = V_total, buffer = buffer, type = type)
